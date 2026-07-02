091a488d1e3a59e8e386ad564bd5b89e6f2e3a86
Akashdeep <akashvohra9877@gmail.com>
2026-07-02 22:00:01 +0530

    feat: add site banner service with Redis and MongoDB integration
    - Created package.json for the site banner service with dependencies on express, ioredis, and mongoose.
    - Implemented the main application logic in src/index.js to handle banner messages with CRUD operations using Redis.
    - Added docker-compose.yml to set up Redis and MongoDB services with persistent storage.

---
+++ 02-setup-local-redis/src/index.js
@@ -0,0 +1,31 @@
+import express from "express";
+import Redis from "ioredis";
+import mongoose, { mongo } from "mongoose";
+
+const app = express();
+
+const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+app.get("/redis", async (req, res) => {
+  const reply = await redis.ping();
+
+  res.json({
+    redis: reply,
+  });
+});
+
+
+app.get("/mongo", async (req, res) => {
+  const url = process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/akash_redis'
+
+  if(mongoose.connect.readyState === 0){
+    await mongoose.connect(url)
+  }
+
+  res.json({mongo : "connected", database : mongoose.connection.name})
+});
+
+
+app.listen(3000, () =>  {
+    console.log(`server is running on port 3000`)
+})

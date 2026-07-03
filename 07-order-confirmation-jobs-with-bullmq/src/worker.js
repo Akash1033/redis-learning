aedfa5d2be57c60ec608c5b55b091057cbcaa4e6
Akashdeep <akashvohra9877@gmail.com>
2026-07-03 23:11:52 +0530

    email queue with bullmq

---
+++ 07-order-confirmation-jobs-with-bullmq/src/worker.js
@@ -0,0 +1,21 @@
+import { Job, Worker } from "bullmq";
+import { connection } from "./queue.js";
+
+const emailWorker = new Worker(
+    'emails',
+    async(job) => {
+        console.log("processing email job...", job.id ,job.name , job.data)
+         (await new Promise((resolve) => setTimeout(resolve, 1500)),
+         console.log('Email job Completed!', job.id, job.name, job.data));
+    },
+    {connection}
+)
+
+emailWorker.on('completed', (job) => {
+    console.log('job completed!', job.id, job.name, job,data);
+});
+
+
+emailWorker.on('failed', (job, err) => {
+    console.log('job failed!',job.id, job.name, job.data, err)
+})
